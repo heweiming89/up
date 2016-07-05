8810b565c9f93050d0bd302c3408e63d9ba8973e
heweiming <heweiming89@163.com>
2016-07-05 13:18:45 +0800

    2016年7月5日 13:18:42

---
+++ src/test/java/cn/heweiming/up/test/CodeTest.java
@@ -0,0 +1,18 @@
+package cn.heweiming.up.test;
+
+/**
+ * Created by heweiming on 2016/7/4.
+ */
+public class CodeTest {
+
+    public static void main(String[] args) {
+        System.out.println();
+        String str = "中国";
+        System.out.println("args = [" + args + "]");
+        System.out.println("str = " + str);
+        System.out.println("args = [" + args + "]");
+        System.out.println("CodeTest.main");
+
+    }
+    
+}
